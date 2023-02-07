ebb849dca6b5ea57a3d60fd5422148b6b0911b72
Learning <88524372+CurryevtWSN@users.noreply.github.com>
2023-02-07 16:05:42 +0800

    Update lung_cancer_eye.py

--- lung_cancer_eye.py
+++ lung_cancer_eye.py
@@ -13,7 +13,7 @@ import joblib
 st.set_option('deprecation.showPyplotGlobalUse', False)
 #%%set title
 st.set_page_config(page_title='Prediction model for Ocular metastasis of lung cancer')
-st.title('Prediction model of ocular metastasis of lung cancer using machine learning methods')
+st.title('Eye metastasis of primary lung cancer: development and verification of predictive model based extreme gradient boost')
 st.sidebar.markdown('## Variables')
 Histopathological_type = st.sidebar.selectbox('Histopathological_type',('Squamous cell carcinoma','Adenocarcinoma','Large cell carcinoma',
                                                                         'Small cell lung cancer','Other non-small cell lung cancer','Unkown'),index=1)
